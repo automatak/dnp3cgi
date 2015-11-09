da534dde3429403f21514b297a0c4bcb414fd0d1
jadamcrain <jadamcrain@gmail.com>
2015-11-09 08:40:36 -0800

    initial version of HexData clasS

---
+++ src/HexData.h
@@ -0,0 +1,40 @@
+
+#ifndef HEXDATA_H
+#define HEXDATA_H
+
+#include <string>
+#include <cstdint>
+#include <memory>
+
+class HexData {
+
+public:
+    HexData(const std::string& hex);
+
+    const uint8_t* Buffer() const;
+    size_t Size() const;
+
+    static std::string Convert(const uint8_t* pBuff, size_t length, bool spaced = false);
+
+private:
+
+    static char ToHexChar(char c);
+
+    static std::string RemoveSpaces(const std::string& hex);
+
+    static void RemoveSpacesInPlace(std::string& s);
+
+    static size_t Validate(const std::string& hex);
+
+    static bool IsHexChar(char i);
+    static bool IsDigit(char i);
+    static bool IsUpperHexAlpha(char i);
+    static bool IsLowerHexAlpha(char i);
+
+    std::string m_input;
+    size_t m_size;
+    std::unique_ptr<uint8_t[]> m_buffer;
+
+};
+
+#endif
